cf6e298da1e71064efec9159b3e49951f79e8549
NinKyuWudd <72558822+ReihanWudd@users.noreply.github.com>
2021-12-09 09:20:29 +0700

    a lot of fixed in here
    and add more table / fixed

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import json
 import os
 import time
+import random
 
+
+os.system('cls')
 print("""██     ██ ███████ ██       ██████  ██████  ███    ███ ███████ 
 ██     ██ ██      ██      ██      ██    ██ ████  ████ ██      
 ██  █  ██ █████   ██      ██      ██    ██ ██ ████ ██ █████   
@@ -77,12 +80,14 @@ def loadfile(file):
             _data = json.load(output)
             return _data
 #untuk function yang menampilkan jadwal dokter, agar tidak menulis ulang pada setiap line nya
-def displaytime(no1,no2,hari,jam):
+def displaytime(no1,no2,hari,hari2,jam,jam2):
     print('\n')
-    
+    print("="*56)
     print("| {:^3} | {:^18} | {:^25} |".format("No","Hari","Jam"))
+    print("-"*56)
     print(f"| {no1:^3} | {hari:^18} | {jam:^25} |")
-    print(f"| {no2:^3} | {hari:^18} | {jam:^25} |")
+    print(f"| {no2:^3} | {hari2:^18} | {jam2:^25} |")
+    print("="*56)
     pilih_jadwal = int(input("Choose your appointment [1/2] = "))
     global servis_program
     global jam_nilai
@@ -101,7 +106,7 @@ def displaytime(no1,no2,hari,jam):
         print("No Schedule")    
     
 #fucntion untuk menampilkan daftar dokter yang ada pada setiap line, agar tidak menulis ulang pada setiap line nya
-def displaydokter(dokter1,dokter2,hari,jam):
+def displaydokter(dokter1,dokter2,hari,hari2,jam,jam2):
     print("_"*50)
     print("   DOKTER\n   1. "+dokter1+"\n   2. "+dokter2)
     pilih_dokter = int(input('Enter your Choise [1/2] = '))
@@ -109,11 +114,11 @@ def displaydokter(dokter1,dokter2,hari,jam):
     global dokter_nilai
     if pilih_dokter == 1:
         dokter_nilai =dokter1
-        displaytime(1,2,hari,jam)
+        displaytime(1,2,hari,jam,hari2,jam2)
 
     elif pilih_dokter == 2:
         dokter_nilai = dokter2
-        displaytime(1,2,hari,jam)
+        displaytime(1,2,hari,jam,hari2,jam2)
 
 #untuk membuat countdown waktu/timer
 def countdown(w):
@@ -123,8 +128,9 @@ def countdown(w):
         print(timer, end="\r")
         time.sleep(1)
         w -= 1
-      
-    print('Your medicine is ready')
+    
+    print('Your medicine is ready, Thanks for your puchase!')
+    
 
 def shoowobat(sirup1,sirup2,pil1,pil2,jmlsir1,jmlsir2,jmlpil1,jmlpil2):
 
@@ -133,12 +139,14 @@ def shoowobat(sirup1,sirup2,pil1,pil2,jmlsir1,jmlsir2,jmlpil1,jmlpil2):
     nilai_sirup2 = sirup2
     nilai_pil1 = pil1
     nilai_pil2 = pil2
-    obat_tersedia="Stok"
-    print("| {:^5} | {:^18} | {:^18} |".format("No","Sirup","Tablet/Kapsul"))
-    print(f"| {1:^5} | {sirup1:^18} | {pil1:^18} |")
-    print(f"| {obat_tersedia:^5} | {jmlsir1:^18} | {jmlpil1:^18} |")
-    print(f"| {2:^5} | {sirup2:^18} | {pil2:^18} |")
-    print(f"| {obat_tersedia:^5} | {jmlsir2:^18} | {jmlpil2:^18} |")
+    print("="*67)
+    print("| {:^5} | {:^18} | {:^5} | {:^18} | {:^5} |".format("No","Sirup","Stok","Tablet/Kapsul","Stok"))
+    print("="*67)
+    print(f"| {1:^5} | {sirup1:^18} | {jmlsir1:^5} | {pil1:^18} | {jmlpil1:^5} |")
+    print("| {:^5} | {:^18} | {:^5} | {:^18} | {:^5} |".format("price",'5000','-','8000','-'))
+    print(f"| {3:^5} | {sirup2:^18} | {jmlsir2:^5} | {pil2:^18} | {jmlpil2:^5} |")
+    print("| {:^5} | {:^18} | {:^5} | {:^18} | {:^5} |".format("price",'3000','-','11000','-'))
+    print("="*67)
     return sirup1,sirup2,pil1,pil2
   
 # print("Nama   :"+user_regis[user_id]["nama"]+"\nServis :"+servis_nilai+"\nPoli   :"+poli_nilai+"\nDokter :"+dokter_nilai+"\nJadwal :"+jam_nilai)
@@ -150,15 +158,16 @@ def regisjanji(janji):
         "Dokter": dokter_nilai,
         "Jadwal": jam_nilai
     })
+    print("="*50)
     return janji
 
 def showapp():
-    print("nama:"+janji_regis[janji_ke]["nama"]+" dokter:"+janji_regis[janji_ke]["Dokter"]+" jadwal"+janji_regis[janji_ke]["Jadwal"])
+    print("="*48+"\n"+"Nama : "+janji_regis[janji_ke]["nama"]+"\n"+"Dokter : "+janji_regis[janji_ke]["Dokter"]+"\n"+"Jadwal : "+janji_regis[janji_ke]["Jadwal"]+"\n"+"="*48)
 
 
 
 def delete(delvar):
-    print(len(delvar))
+    print("Total Jadwal anda :"+str(len(delvar)))
     global janji_ke
     try:
         jan_input = int(input("Janji ke? = "))
@@ -198,7 +207,7 @@ def keluar():
             print("your insert wrong word!!")
 
 #membuat variabel untuk menampung file .json
-os.system('clear')
+
 DATA_USER = "data.json"
 user_regis = loadfile(DATA_USER)
 
@@ -230,45 +239,56 @@ jmlsir1 = 20
 jmlsir2 = 20
 jmlpil1 = 20
 jmlpil2 = 20
+hrg_1a = 5000
+hrg_1b = 8000
+hrg_2a = 3000
+hrg_2b = 11000
+
 main_choise = True
 while main_choise:
+
     #pertanyaan untuk memilih jasa yang disediiakan
-    print('_'*20+"WHAT CAN I HELP YOU?"+'_'*20)
+    print('-'*20+"HELLO WHAT CAN I HELP YOU?"+'-'*20)
     print("""
-        1. Appointment
-        2. Pharmacy
-        3. Cancel Appointment
+                ===============================
+                |    1. Appointment           |
+                |    2. Pharmacy              |
+                |    3. Cancel Appointment    |
+                ===============================
     """)
-    pilih_servis = input('Enter your Choise [1/2/3] = ')
+    pilih_servis = input('                  Enter your Choise [1/2/3] = ')
 
     #looping untuk service program yang dipilih
     servis_program = True
     while servis_program:
         #kondisi keetika user memilih jasa Appointment
         if pilih_servis == "1":
-            servis_nilai = "Appointment"
-            print('Choose your complaint')
+            servis_nilai = "Appointment" 
+            print('\n') 
+            print('-'*20+'Choose your complaint'+'-'*20)
             print("""
-        1. POLI MATA
-        2. POLI KULIT
-        3. POLI PENYAKIT DALAM
-        4. POLI SARAF
+               =================================
+               |  1. POLI MATA                 |
+               |  2. POLI KULIT                |
+               |  3. POLI PENYAKIT DALAM       |
+               |  4. POLI SARAF                |  
+               ================================= 
             """)
-            pilih_poli = int(input("Enter your Choise [1/2/3/4] = "))
+            pilih_poli = int(input("                Enter your Choise [1/2/3/4] = "))
             print('\n')
             #note : di displaydokter itu tinggal mengganti isinya saja ,untuk tampilan akan menyesuaikan isi itu
             if pilih_poli == 1:
                 poli_nilai = "POLI MATA"
-                displaydokter("dr. Putu Budi Sucitro","dr. Angel Benny","Senin - Kamis","07:30 - 13:00")
+                displaydokter("dr. Putu Budi Sucitro","dr. Angel Benny","Senin - Kamis","07:30 - 13:00","Jumat - Sabtu","18:00 - 21:00")
             elif pilih_poli == 2:
                 poli_nilai = "POLI KULIT"
-                displaydokter("dr. Ciko edo","dr. Angel Benny","Jumat - Minggu","16:00 - 22:00")
+                displaydokter("dr. Ciko edo","dr. Angel Benny","Senin - Rabu","08:00 - 14:00","Jumat - Minggu","16:00 - 22:00")
             elif pilih_poli == 3:
                 poli_nilai = "POLI PENYAKIT DALAM"
-                displaydokter("dr. Jokowi Dodo" ,"dr. Megawati","Senin - Rabu","08:00 - 11.00")
+                displaydokter("dr. Jokowi Dodo" ,"dr. Megawati","Senin - Rabu","08:00 - 11.00","Kamis - Sabtu","15:00 - 19:00")
             elif pilih_poli == 4:
                 poli_nilai = "POLI SARAF"
-                displaydokter("dr. Wudd ","dr. Vania","Jumat - Sabtu","19:00 - 22:00")
+                displaydokter("dr. Wudd ","dr. Vania","Senin - Rabu","09;00 - 13:00","Jumat - Sabtu","19:00 - 22:00")
             else:
                 print("POLI SUDAH TUTUP")
 
@@ -286,6 +306,8 @@ while main_choise:
         Do you have a Doctor's Presciption ?
         [y/n]: """)
             if resep_dok == "y":
+                hrg_resep = random.randint(5,300)
+                print("total Rp."+str(hrg_resep)+"000")
                 countdown(int(10))
                 keluar()
             elif resep_dok == "n":
@@ -297,25 +319,35 @@ while main_choise:
 
                     if jumlah > 20:
                         print("Out of stock")
-                    elif pilih_obat == "1a":
+                    elif pilih_obat == "1a" and jmlsir1 != 0:
                         jmlsir1 -= jumlah
-                        print("Complaint "+penyakit+" Medicine "+nilai_sirup1)
-                        print("Your purchase has been completed")
-                    elif pilih_obat == "1b":
+                        
+                        print("Your "+penyakit+" Medicine "+nilai_sirup1)
+                        print("total Rp." +str(hrg_1a * jumlah))
+                        print("Purchase has been completed")
+                    elif pilih_obat == "1b" and jmlpil1 != 0:
                         jmlpil1 -= jumlah
-                        print("Complaint "+penyakit+" Medicine "+nilai_pil1)
-                        print("Your purchase has been completed")
-                    elif pilih_obat == "2a":
+                        
+                        print("Your "+penyakit+" Medicine "+nilai_pil1)
+                        print("total Rp." +str(hrg_1b * jumlah))
+                        print("Purchase has been completed")
+                    elif pilih_obat == "2a" and jmlsir2 != 0:
                         jmlsir2 -= jumlah
-                        print("Complaint "+penyakit+" Medicine "+nilai_sirup2)
-                        print("Your purchase has been completed")
-                    elif pilih_obat == "2b":
+                        
+                        print("Your "+penyakit+" Medicine "+nilai_sirup2)
+                        print("total Rp." +str(hrg_2a * jumlah))
+                        print("Purchase has been completed")
+                    elif pilih_obat == "2b" and jmlpil2 != 0:
                         jmlpil2 -= jumlah
-                        print("Complaint "+penyakit+" Medicine "+nilai_pil2)
-                        print("Your purchase has been completed")
+                    
+                        print("Your "+penyakit+" Medicine "+nilai_pil2)
+                        print("total Rp." +str(hrg_2b * jumlah))
+                        print("Purchase has been completed")
+                    else:
+                        print("you insert wrong input !!")
 
                     while True:
-                        blanja_obat_exit = input("buy again? y/n = ")
+                        blanja_obat_exit = input("Buy again? y/n = ")
                         if blanja_obat_exit == "y":
                             break
                         elif blanja_obat_exit == "n":
@@ -330,6 +362,7 @@ while main_choise:
             keluar()
         else:
             print("You entered the wrong number")
+            keluar()
 
 print("""████████ ██   ██  █████  ███    ██ ██   ██ ███████ 
    ██    ██   ██ ██   ██ ████   ██ ██  ██  ██      
